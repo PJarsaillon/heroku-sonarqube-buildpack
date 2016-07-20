3022494d0d7e4035879e3bf095a88f3d4e7e28a3
Pierre Jarsaillon <pierre.jarsaillon@applidium.com>
2016-07-20 14:50:54 +0200

    fix setup script

--- setupSonar.sh
+++ setupSonar.sh
@@ -13,6 +13,5 @@ sed -i -e "s/@DB_PASSWORD@/${JDBC_DATABASE_PASSWORD}/g" ./conf/sonar.properties
 echo "       sonar.jdbc.url is set to ${JDBC_DATABASE_URL}"
 sed -i -e "s/@BD_URL@/${JDBC_DATABASE_URL}/g" ./conf/sonar.properties
 
-JDBC_DATABASE_URL
 echo "-----> Start sonar.sh start"
 ./bin/linux-x86-64/sonar.sh start
