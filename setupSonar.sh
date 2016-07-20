e22af65e8b56328d11668b93c6ee686ae1ed00ee
Pierre Jarsaillon <pierre.jarsaillon@applidium.com>
2016-07-20 14:47:07 +0200

    add bdd config

--- setupSonar.sh
+++ setupSonar.sh
@@ -4,5 +4,15 @@ echo "-----> Set sonar.properties"
 echo "       sonar.web.port is set to ${PORT}"
 sed -i -e "s/@PORT@/${PORT}/g" ./conf/sonar.properties
 
+echo "       sonar.jdbc.username is set to ${JDBC_DATABASE_USERNAME}"
+sed -i -e "s/@DB_USERNAME@/${JDBC_DATABASE_USERNAME}/g" ./conf/sonar.properties
+
+echo "       sonar.jdbc.password is set to ${JDBC_DATABASE_PASSWORD}"
+sed -i -e "s/@DB_PASSWORD@/${JDBC_DATABASE_PASSWORD}/g" ./conf/sonar.properties
+
+echo "       sonar.jdbc.url is set to ${JDBC_DATABASE_URL}"
+sed -i -e "s/@BD_URL@/${JDBC_DATABASE_URL}/g" ./conf/sonar.properties
+
+JDBC_DATABASE_URL
 echo "-----> Start sonar.sh start"
 ./bin/linux-x86-64/sonar.sh start
